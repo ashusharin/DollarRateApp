ceaf861d3a2221bc569bce50ce1956ef542700b0
Alexandr Shusharin <ashusharin.dev@gmail.com>
2022-02-04 16:16:40 +0500

    Add dependency in custom application

--- app/src/main/java/com/shusharin/dollarrateapp/core/RateApp.kt
+++ app/src/main/java/com/shusharin/dollarrateapp/core/RateApp.kt
@@ -1,10 +1,63 @@
 package com.shusharin.dollarrateapp.core
 
 import android.app.Application
+import com.shusharin.dollarrateapp.data.RateDataListToDomainMapper
+import com.shusharin.dollarrateapp.data.RateRepository
+import com.shusharin.dollarrateapp.data.net.RateCloudDataSource
+import com.shusharin.dollarrateapp.data.net.RateCloudListMapper
+import com.shusharin.dollarrateapp.data.net.RateCloudToDataMapper
+import com.shusharin.dollarrateapp.data.net.RateService
+import com.shusharin.dollarrateapp.domain.BaseRateDataListToDomainMapper
+import com.shusharin.dollarrateapp.domain.BaseRateDataToDomainMapper
+import com.shusharin.dollarrateapp.domain.RateInteractor
+import com.shusharin.dollarrateapp.ui.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RateApp : Application() {
+    private companion object {
+        const val BASE_URL = "http://cbr.ru/scripts/"
+    }
+
+    lateinit var mainViewModel: MainViewModel
 
     override fun onCreate() {
         super.onCreate()
+        val client = OkHttpClient.Builder()
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .readTimeout(1, TimeUnit.MINUTES)
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(SimpleXmlConverterFactory.create())
+            .build()
+        val service = retrofit.create(RateService::class.java)
+        val cloudDataSource = RateCloudDataSource.Base(service)
+        val rateCloudToDataMapper = RateCloudToDataMapper.Base()
+        val rateCloudMapper = RateCloudListMapper.Base(rateCloudToDataMapper)
+        val personRepository = RateRepository.Base(
+            cloudDataSource,
+            rateCloudMapper
+        )
+        val personsInteractor = RateInteractor.Base(
+            personRepository,
+            BaseRateDataListToDomainMapper(BaseRateDataToDomainMapper())
+        )
+        val communication = RateUiCommunication.Base()
+        mainViewModel = MainViewModel(
+            personsInteractor,
+            communication,
+            BaseRateDomainListToUiMapper(ResourceProvider.Base(this),
+                BaseRateDomainToUiMapper()),
+
+            )
     }
 }
